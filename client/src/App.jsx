6963cd11ee3c9f1201d7f067684dc24035ee6f95
Abhi-lash27 <babhilash27@gmail.com>
2023-12-19 20:37:19 +0530

    Created Doctor Profile and modified the navbar.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,10 @@ import SignUp from './components/SignUp'
 import DoctorDashboard from './dashboard/DoctorDashboard'
 import NurseDashboard from './dashboard/NurseDashboard'
 import PatientDashboard from './dashboard/PatientDashboard'
+<<<<<<< Updated upstream
+=======
+import DoctorProfile from './pages/Doctor/DoctorProfile'
+>>>>>>> Stashed changes
 
 const App = () => {
   return (
@@ -16,6 +20,10 @@ const App = () => {
           <Route path='/signup' exact element={<SignUp />} />
           <Route path='/doctor' exact element={<DoctorDashboard />} />
           <Route path='/nurse' exact element={<NurseDashboard />} />
+<<<<<<< Updated upstream
+=======
+          <Route path='/doctor/:activepage' exact element={<DoctorProfile/>} />
+>>>>>>> Stashed changes
           <Route path='/patient' exact element={<PatientDashboard />} />
         </Routes>
     </BrowserRouter>
---
+++ client/src/pages/Doctor/DoctorProfile.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+import DoctorNav from '../../components/Navbar/Doctor-Nav'
+import HeadBanner from '../../components/Banner/HeadBanner'
+import UserSidebar from '../../components/UserProfile/UserSidebar'
+import AccountSettings from '../../components/UserProfile/AccountSettings'
+import './DoctorProfile.css'
+import ChangePassword from '../../components/UserProfile/ChangePassword'
+import Notifications from '../../components/UserProfile/Notifications'
+
+const DoctorProfile = () => {
+
+  const {activepage} = useParams()
+  // alert(activepage)
+  return (
+    <div className='docprofile'>
+      <DoctorNav/>
+      <HeadBanner 
+      bannerimage='https://source.unsplash.com/random?wallpapers'
+      heading='My profile'
+      />
+      
+      {/* DoctorProfile, Showing {activepage} */}
+
+      <div className='docprofilein'>
+        <div className='left'>
+          <UserSidebar activepage={activepage}/>
+        </div>
+        <div className='right'>
+          {activepage === 'accountsettings' && <AccountSettings/>}
+          {activepage === 'changepassword' && <ChangePassword/> }
+          {activepage === 'notify' && <Notifications/> }
+        </div>
+      </div>
+      </div>
+  )
+}
+
+export default DoctorProfile
