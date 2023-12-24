f54b92a94e0f06a47da7634dabaea4f70ecb8a9c
AbdulkalamAasath <142470162+AbdulkalamAasath@users.noreply.github.com>
2023-12-24 21:31:50 +0530

    updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
+import Patient from './pages/Nurse/Patient'
 import Home from './pages/Home'
 import SignUp from './components/SignUp'
 import DoctorDashboard from './dashboard/DoctorDashboard'
@@ -10,25 +10,11 @@ import PatientDashboard from './dashboard/PatientDashboard'
 import PatientProfile from './pages/Patient/PatientProfile'
 import PChangePassword from './components/UserProfile/Patient/PChangePassword'
 import PNotifications from './components/UserProfile/Patient/PNotifications'
-import DoctorProfile from './pages/Doctor/DoctorProfile'
-import NurseProfile from './components/Navbar/Nurse/NurseProfile'
-import Main from './components/Appointment/Patient/Main'
-
-
-import PatientProfile from './pages/Patient/PatientProfile'
-import PChangePassword from './components/UserProfile/Patient/PChangePassword'
-import PNotifications from './components/UserProfile/Patient/PNotifications'
-
 
 import DoctorProfile from './pages/Doctor/DoctorProfile'
 import NurseProfile from './components/Navbar/Nurse/NurseProfile'
-
-
 const App = () => {
-
-  
-
-  return ( 
+  return (
     <>
      <BrowserRouter>
         <Routes>
@@ -39,6 +25,7 @@ const App = () => {
           <Route path='/nurse/profile' exact element={<NurseProfile />} />
           <Route path='/nurse/patient' exact element={<Patient />} />
           <Route path='/patient' exact element={<PatientDashboard />} />
+
           <Route path='/patient/:activepage' exact element={<PatientProfile/>} />
           <Route path='/patient/:activepage' exact element={<PChangePassword/>}/>
           <Route path='/doctor/:activepage' exact element={<DoctorProfile />} />
