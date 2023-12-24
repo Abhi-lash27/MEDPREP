6cdcfb47b320ac57854e5701eecdd9fa7100d6b1
AbdulkalamAasath <142470162+AbdulkalamAasath@users.noreply.github.com>
2023-12-24 21:02:21 +0530

    pull data

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,15 +6,15 @@ import SignUp from './components/SignUp'
 import DoctorDashboard from './dashboard/DoctorDashboard'
 import NurseDashboard from './dashboard/NurseDashboard'
 import PatientDashboard from './dashboard/PatientDashboard'
-<<<<<<< HEAD
+
 import PatientProfile from './pages/Patient/PatientProfile'
 import PChangePassword from './components/UserProfile/Patient/PChangePassword'
 import PNotifications from './components/UserProfile/Patient/PNotifications'
 
-=======
+
 import DoctorProfile from './pages/Doctor/DoctorProfile'
 import NurseProfile from './components/Navbar/Nurse/NurseProfile'
->>>>>>> 8b7b6e9c1c780d343c9444ba23c8047aab61d602
+
 
 const App = () => {
   return (
@@ -28,12 +28,9 @@ const App = () => {
           <Route path='/nurse/profile' exact element={<NurseProfile />} />
           <Route path='/nurse/patient' exact element={<Patient />} />
           <Route path='/patient' exact element={<PatientDashboard />} />
-<<<<<<< HEAD
           <Route path='/patient/:activepage' exact element={<PatientProfile/>} />
           <Route path='/patient/:activepage' exact element={<PChangePassword/>}/>
-=======
           <Route path='/doctor/:activepage' exact element={<DoctorProfile />} />
->>>>>>> 8b7b6e9c1c780d343c9444ba23c8047aab61d602
         </Routes>
     </BrowserRouter>
     </>
