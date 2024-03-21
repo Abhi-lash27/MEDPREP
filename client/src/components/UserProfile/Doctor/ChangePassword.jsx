02e1c14a14c6a171ae2bd1814756b0638f3eb957
Akalya1609 <akalyasubramaniane16@gmail.com>
2024-03-21 23:27:20 +0530

    updated change password

--- client/src/components/UserProfile/Doctor/ChangePassword.jsx
+++ client/src/components/UserProfile/Doctor/ChangePassword.jsx
@@ -1,9 +1,6 @@
 import React from 'react'
 import { Trans, useTranslation } from 'react-i18next';
 import i18next from 'i18next';
-
-
-import React from 'react';
 import './ChangePassword.css';
 
 const ChangePassword = () => {
